7059cce6def4944abcab3396540376dcf9b155ff
bushand1990 <156022014+bushand1990@users.noreply.github.com>
2025-01-29 10:12:44 -0600

    Updated github link.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-links">
-        <a href="https://github.com/drewbush1990/" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/AndrewBush-Tech/" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
         <a href="https://www.linkedin.com/in/andrew-bush-22b09b148/" target="_blank" rel="noopener noreferrer">
